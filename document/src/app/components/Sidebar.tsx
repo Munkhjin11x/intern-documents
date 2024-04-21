39f478cc098bcf31986893d8786fb9ab1726c356
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-04-21 15:54:59 +0800

    sss

--- document/src/app/components/Sidebar.tsx
+++ document/src/app/components/Sidebar.tsx
@@ -1,25 +1,22 @@
-'use client'
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+"use client";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { useState } from "react";
-import Logo from "../assets/Logo"
-import Pinecone from "../assets/Pinecone"
+import Logo from "../assets/Logo";
+import Pinecone from "../assets/Pinecone";
 import { graphql } from "../constant/graphql";
 import { NX } from "../constant/nxMonorepo";
 import { test } from "../constant/testing";
 import { pr } from "../constant/pullreq";
 import { useRouter } from "next/navigation";
 
-
 const Sidebar = () => {
-  const router = useRouter()
+  const router = useRouter();
   const [showGraphQL, setShowGraphQL] = useState(false);
   const [showNX, setShowNx] = useState(false);
   const [showPr, setShowPr] = useState(false);
   const [showTest, setShowTest] = useState(false);
   const [activePath, setActivePath] = useState("");
-
-
 
   const toggleGraphQL = () => {
     setShowGraphQL(!showGraphQL);
@@ -33,48 +30,71 @@ const Sidebar = () => {
   const toggleTest = () => {
     setShowTest(!showTest);
   };
-  const renderIcon = (showSection:any) => {
+  const renderIcon = (showSection: any) => {
     return showSection ? (
       <KeyboardArrowDownIcon sx={{ color: "#4ADF7E", height: "24px" }} />
     ) : (
       <ArrowForwardIosIcon sx={{ color: "#4ADF7E", height: "16px" }} />
     );
   };
-  const handlePathClick = (path:any) => {
-    setActivePath(path);
-    router.push(path);
-  };
-
   return (
-    <div className=" h-screen">
-        <div className="flex  border-b-[1px] border-r-[1px] border-[#2F2F2F]  ">
+    <div className=" h-[1000px]">
+      <div className="flex  border-b-[1px] border-r-[1px] border-[#2F2F2F]  ">
         <div className="flex h-[64px] w-[299px] gap-2 justify-center items-center">
-        <Logo/>
-            <Pinecone/>
+          <Logo />
+          <Pinecone />
         </div>
-        </div>
-        <div className=" border-r-[1px] border-[#2F2F2F] h-[1050px] ">
-          <div className="text-white px-5 py-2 flex flex-col gap-[4px]">
-          <p onClick={toggleGraphQL} className="cursor-pointer text-[15px] font-medium hover:bg-[#252525] rounded-sm"> {renderIcon(showGraphQL)}Graphql </p>
+      </div>
+      <div className=" border-r-[1px] border-[#2F2F2F] h-[900px] ">
+        <div className="text-white px-5 py-2 flex flex-col gap-[4px]">
+          <p
+            onClick={toggleGraphQL}
+            className="cursor-pointer text-[15px] font-medium hover:bg-[#252525] rounded-sm"
+          >
+            {" "}
+            {renderIcon(showGraphQL)}Graphql{" "}
+          </p>
           {showGraphQL && (
-            <ul className='flex flex-col gap-[2px] border-l-[1px] border-[#2F2F2F]'>
-              {graphql.map((e)=>(
-                <li onClick={()=> router.push(`${e.path}`)} className="px-[27px] text-[14px] text-[#2F2F2F] hover:text-white cursor-pointer hover:bg-[#252525] ease-in-out duration-300">{e.title} <ArrowForwardIosIcon sx={{color:'#4ADF7E',height:"16px"}}/></li>
+            <ul className="flex flex-col gap-[2px] border-l-[1px] border-[#2F2F2F]">
+              {graphql.map((e) => (
+                <li
+                  onClick={() => router.push(`${e.path}`)}
+                  className="px-[27px] text-[14px] text-[#2F2F2F] hover:text-white cursor-pointer hover:bg-[#252525] ease-in-out duration-300"
+                >
+                  {e.title}{" "}
+                  <ArrowForwardIosIcon
+                    sx={{ color: "#4ADF7E", height: "16px" }}
+                  />
+                </li>
               ))}
             </ul>
           )}
-                <p onClick={toggleTest} className="cursor-pointer text-[15px]  hover:bg-[#252525] font-medium">   {renderIcon(showTest)}Testing</p>
+          <p
+            onClick={toggleTest}
+            className="cursor-pointer text-[15px]  hover:bg-[#252525] font-medium"
+          >
+            {" "}
+            {renderIcon(showTest)}Testing
+          </p>
           {showTest && (
-            <ul className='flex flex-col gap-[2px] border-l-[1px] border-[#2F2F2F]'>
-              {test.map((e)=>(
-                <li onClick={()=> router.push(`${e.path}`)} className="px-[27px] cursor-pointer hover:bg-[#252525] text-[14px] text-[#2F2F2F] hover:text-white c ease-in-out duration-300">{e.title} <ArrowForwardIosIcon sx={{color:'#4ADF7E',height:"16px"}}/></li>
+            <ul className="flex flex-col gap-[2px] border-l-[1px] border-[#2F2F2F]">
+              {test.map((e) => (
+                <li
+                  onClick={() => router.push(`${e.path}`)}
+                  className="px-[27px] cursor-pointer hover:bg-[#252525] text-[14px] text-[#2F2F2F] hover:text-white c ease-in-out duration-300"
+                >
+                  {e.title}{" "}
+                  <ArrowForwardIosIcon
+                    sx={{ color: "#4ADF7E", height: "16px" }}
+                  />
+                </li>
               ))}
             </ul>
           )}
-          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
